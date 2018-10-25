076610e6cdfc9719593a9f9cb7dcba7faf811f4f
Jay Lohokare <jaylohokare@gmail.com>
2018-10-25 19:00:44 -0400

    Load counter hosted on eth in webapp

--- solidityWebInteraction/README.txt
+++ solidityWebInteraction/README.txt
@@ -1,2 +1,23 @@
 Start a new chain:
-geth --datadir .\myChainData\ --nodiscover --rpc --rpccorsdomain "*"
+geth init .\genesis.json --datadir myChainData
+
+Create a new wallet account, allocate ethere:
+1. geth --datadir myChainData account new
+The command returns address
+Use this address and add it to genesis.json in alloc
+(Prefix with 0x)
+
+geth --datadir .\myChainData\ --rpc --rpccorsdomain "http://127.0.0.1:8545" --nodiscover 
+
+Ethereum wallet should now connect to private net when opened
+
+Adding ether to wallet ->
+geth attach ipc://./pipe/geth.ipc
+miner.setEtherbase(eth.accounts[0])
+miner.start(1)
+
+
+Creating a contract:
+Go to etherm wallet and create a new contract (Copy paste the code)
+Copy the address and ABI interface into html
+Make sure to clone Web3.js 
--- solidityWebInteraction/webNumberCounter.sol
+++ solidityWebInteraction/webNumberCounter.sol
@@ -10,26 +10,26 @@ contract MyContract{
     address creator;
     uint256 myNumber;
 
-    function MyContract() public{
+    constructor (MyContract) public {
         creator = msg.sender;
         myNumber = 3;
     }
 
-    function getCreator() public  view returns(address){
+    function getCreator()  public  view returns(address){
         return creator;
     }
 
-    function getNumber() public view  returns(uint256){
+    function getNumber() public  view  returns(uint256){
         return myNumber;
     }
 
-    function setNmber(uint256 newNmber) public {
+    function setNmber(uint256 newNmber) public  {
         myNumber = newNmber;
     } 
 
     function kill() public{
         if(msg.sender == creator){
-            suicide(creator);
+            selfdestruct(creator);
         }
     }
     
